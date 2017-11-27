8084ad80a63025560775fd547065c00163cae29f
cpr <cpr@voxteneo.asia>
2017-11-27 10:29:43 +0700

    Release 0.1.0

---
+++ PhrClient.Wpf.Example/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace PhrClient.Wpf.Example
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+            base.OnStartup(e);
+            var token =
+                "BP+Sn+NbLoLaK8uNkfax9B5TzDRNxvfCtdbB44duLcRF3IfymF1tCydVq3CSiyp50286VP0s3NFcR0zGc/Ev289skYSdS1g2h+9Th+7fGoKBS+RcpW0Bj+VOmr+48ROqAsjofD+CmRxJn8ZX8WLNoMQA9g1aWKx8H5jCz4tk2ezlILsr3I+E+uJU01Y/nP0B/NsKrgNgj9uAnLlBltU37E8H+xM5bxNG0fG/YPpmbVcWTK8Kc7aVcE6rFU/rbkYvzuHfOe2oQIgTOxLPlLwb0JfLdzB52UXg8qXcDGBpMm7J36ozizXmEHKTsqrAcEZCCgKzbgk4NdTpv0NXHqjoNx/HKUdp96zZZBUqIQ6w0OY1jKIjgA23xvWeywrqMtvKwiUFflcGgSY7Jyux0pzBILWAsUTCdKRVT8Rn2q132W9wLYRRRTdOM/VNLmysFWE2hhjOucAi7CFV3wAHibTwllOaHPYEvHo4+k3WSJS4n/4UVsN2fdUyy8rz3e61WhkAUMcn1dvhJtCdD485qqMwqifoTgpu2wi384FHgKJAcqraqkX3Vq4RnLj0whP/gDqZ4DlSRu05Cb5z7OCAYXYwiKpPu6H57UzCG1YeDO+GLHjFS6yiluOoyiK8f7F9ztzCufL+ZTtO8UpMkBPhkkACYiG0wsTbiaQZksq0pgBgztHpnc/n4mGnpjmF6FB15L58ws6khQejslBdH7J5mUHW6jeBOWawrG00JAGVvg2om/hVszet48s1WUrZASzSSU9oelhWpivCO0HSOuHNYLJ//SJOznC7s27GdYlZdTTZgXoHZTgMXZfYPn0ZpVULt9A+g0mzzacFCWdlPYC7OOczqaJMzgr1ExCVsptpYUAgZjVxVI7Afdc//ND88JY98T4jlFaNXgbRX6TH9GxtSymrhp+GsCi000kkXvp9ce0s3/frGzjl/MxluYhFoKgWUVwsYN3lkD8jbmNynvjDQmnryLQ0lSgVSXB8gtm0OmlYIGXOjZoX864/uc57fvk8BcaZ5dO6ShzLCD74/xGeYE+i+otkziHvvMsx0gzneF56BMAgiSN/mCIvWKcIN6IuzCrOE17xlKnk6lTNtGAOo0LEV9kkM1NFqWeMNYxzAvJW2ku1scLRnfbyZLLaXMPrNO71c+y35zDAjic48IaPKRs72e+dEBTM49k2LR4TwrV62XgVTq7GacSd3/PbHErA6p9V7hvadVEkZIpYRkekKlAZKUtgeYaIZALariletAMrY8UxQLbSwYZ0JFx9c7TgpBFo+5qizggsQqRuT/lQCFwJMQ==";
+
+            ClientContext.AddConfiguration(new PatientConfigurationItem()
+            {
+                Token = token
+
+            });
+            ClientContext.Start();
+        }
+    }
+}
---
+++ PhrClient.Wpf.Example/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Newtonsoft.Json;
+
+namespace PhrClient.Wpf.Example
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private ClientContext _ctx;
+        private WpfClientContext _wpfContext;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            _ctx = new ClientContext();
+            _wpfContext = new WpfClientContext();
+            _ctx.OnRecieveDetailEcg += CtxOnOnRecieveDetailEcg;
+            _ctx.OnRecieveMessage +=CtxOnOnRecieveMessage;
+            _ctx.GetDetailEcg("d05a1a33-159b-47f7-9766-e6f5ca984b36", "9e7bcd3c-e252-4ff1-866d-a449590365b4");
+
+        }
+
+        private void CtxOnOnRecieveMessage(object sender, MessageArgs messageArgs)
+        {
+            var context = sender as ClientContext;
+            if (messageArgs.Type == MessageType.PelayananEcgId)
+            {
+                context.HasReadMessage(messageArgs.Message);
+            }
+        }
+
+        public static void Timeout(Dispatcher dispatcher, Action func, DispatcherPriority dispatcherPriority = DispatcherPriority.ContextIdle)
+        {
+            if (!dispatcher.CheckAccess())
+            {
+                dispatcher.BeginInvoke(dispatcherPriority, func);
+            }
+            else
+                func.Invoke();
+
+        }
+        private void CtxOnOnRecieveDetailEcg(object sender, DetailEcgArgs detailEcgArgs)
+        {
+            Timeout(Dispatcher, () =>
+            {
+                var grid = 20;
+                foreach (var bufferEcgViewModel in detailEcgArgs.Result.Data.Signals)
+                {
+                    var chart = new CoreChartFlat();
+                    wrapPanel.Children.Add(chart);
+                    chart.Height = 300;
+                    chart.Width = ActualWidth - 200;
+                    chart.Y = 8 * grid;
+                    chart.X = 250 * grid;
+
+                    chart.Points = new List<Point>();
+                    chart.UseAnimation = true;
+                    var data = JsonConvert.DeserializeObject<List<object>>(bufferEcgViewModel.Decode);
+                    var decode = string.Join(",", data.Select(n => n.ToString()));
+                    var item = decode.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                    var max = item.Skip(1).Max(n => Convert.ToInt16(n)) + 20;
+                    var scale = chart.Y / max;
+                    chart.BackgroundChartBrush = new SolidColorBrush(Color.FromArgb(63, 73, 112, 189));
+                    var i = 0;
+                    foreach (var s in item.Skip(1))
+                    {
+                        chart.Points.Add(new Point(i, scale * Convert.ToInt16(s)));
+                        i++;
+                    }
+                    var a = chart.Points.FirstOrDefault();
+                    var X = chart.Points.Max(n => n.X);
+                    chart.Points.Add(new Point(X, 0));
+                    chart.Points.Add(new Point(0, 0));
+                    chart.Draw(chart.LastSize);
+                }
+
+            });
+
+        }
+    }
+}
